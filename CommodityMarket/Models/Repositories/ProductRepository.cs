7e75cf6e17d12569b3092153ebeb705e7ba21f5d
Никита Куваев <likeaboss13371@gmail.com>
2021-11-12 12:47:03 +0200

    Logging Add

--- CommodityMarket/Models/Repositories/ProductRepository.cs
+++ CommodityMarket/Models/Repositories/ProductRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CommodityMarketApi.Models.Entities;
 using Dapper;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,6 +11,12 @@ namespace CommodityMarketApi.Models.Repositories
 {
     public class ProductRepository
     {
+        private static ILogger<ProductRepository> _logger;
+        public ProductRepository(ILogger<ProductRepository> logger)
+        {
+            _logger = logger;
+        }
+
         static string connStr = "Data Source = SQL5063.site4now.net; Initial Catalog = db_a7c5be_sellboarddb; User Id = db_a7c5be_sellboarddb_admin; Password=qwerty009";
         public static List<Product> GetProducts()
         {
@@ -21,12 +28,14 @@ namespace CommodityMarketApi.Models.Repositories
                     db.Open();
                     var query = "EXEC [dbo].[GetProducts]";
                     products = db.Query<Product>(query).ToList();
+                    _logger.LogInformation($"Get data has been accesed {DateTime.Now}");
                 }
                 catch (Exception ex)
                 {
                     throw ex;
                 }
             }
+
             return products;
         }
 
@@ -41,12 +50,14 @@ namespace CommodityMarketApi.Models.Repositories
                     var query = "EXEC [dbo].[GetProductsById] @id";
                     var val = new { id = id };
                     products = db.Query<Product>(query).ToList();
+                    _logger.LogInformation($"Get data has been accesed {DateTime.Now}");
                 }
                 catch (Exception ex)
                 {
                     throw ex;
                 }
             }
+
             return products;
         }
     }
